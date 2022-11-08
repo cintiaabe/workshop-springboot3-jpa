2620efdd089082c00ab3a5f67c61c90169cb4868
Cintia <cintia.rossetto@gmail.com>
2022-11-08 20:20:40 -0300

    Payment, one-to-one association

--- src/main/java/curso/curso/config/TestConfig.java
+++ src/main/java/curso/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import curso.curso.entities.Category;
 import curso.curso.entities.Order;
 import curso.curso.entities.OrderItem;
+import curso.curso.entities.Payment;
 import curso.curso.entities.Product;
 import curso.curso.entities.User;
 import curso.curso.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
