012c32877e46ceffa5a420ef6558f1d98f438375
Cintia <cintia.rossetto@gmail.com>
2022-11-07 21:48:39 -0300

    Product-OrderItem one-to-many association - Part 2

--- src/main/java/curso/curso/entities/OrderItem.java
+++ src/main/java/curso/curso/entities/OrderItem.java
@@ -3,6 +3,8 @@ package curso.curso.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import curso.curso.entities.pk.OrderItemPk;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -15,7 +17,7 @@ public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPk id;
+	private OrderItemPk id = new OrderItemPk();
 	
 	private Integer quantity;
 	private Double price;
@@ -30,6 +32,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
